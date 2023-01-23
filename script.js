1d78837130026a273d36d3abff79e384ab0afeb5
Arthuremilio <arthur.ecf@hotmail.com>
2023-01-23 20:44:45 -0300

    env

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-let canvas = document.getElementById("snake"); 
-let context = canvas.getContext("2d"); 
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 let score = document.getElementById('score')
 snake[0] ={
-    x: 8 * box,
-    y: 8 * box
+    x: 7 * box,
+    y: 7 * box
 }
 let direction = "right";
 let food ={
-    x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
+    x: Math.floor(Math.random() * 13 + 1) * box,
+    y: Math.floor(Math.random() * 13 + 1) * box
 }
 
 function criarBG(){
     context.fillStyle = "black";
-    context.fillRect(0, 0, 16*box, 16*box); 
+    context.fillRect(0, 0, 14*box, 14*box,); 
 }
+
 
 function criarCobrinha (){
     for(i = 0; i < snake.length; i++){
         context.fillStyle = "blue";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box,);
         let upscore = 10 * snake.length
         score.innerHTML = `SCORE ${upscore}`
-        console.log(upscore)
-
     }
 }
 
@@ -38,17 +37,39 @@ document.addEventListener('keydown', update);
 
 function update(event){
     if(event.keyCode == 37 && direction != 'right') direction = 'left';
-    if(event.keyCode == 38 && direction != 'down') direction = 'up';
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';  
     if(event.keyCode == 39 && direction != 'left') direction = 'right';
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
+
+var metaChar = false;
+var exampleKey = 38;
+
+document.addEventListener('keyup', keyEvent)
+function keyEvent(event) {
+    if(event.keyCode == 38 && direction != 'down') direction = 'up';   {
+        const btn = document.querySelector("#up")
+        btn.click();
+      console.log("funcionando")
+      metaChar = false;
+    }
+  }
+
+function metaKeyUp (event) {
+  var key = event.keyCode || event.which;
+
+  if (key == exampleKey) {
+    metaChar = false;
+  }
+}
+
 function iniciarJogo(){    
 
-    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0
-    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box
-    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0
-    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
+    if(snake[0].x > 13*box && direction == "right") snake[0].x = 0
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 14 * box
+    if(snake[0].y > 13*box && direction == "down") snake[0].y = 0
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 14 * box
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo)
@@ -77,8 +98,8 @@ function iniciarJogo(){
     }
     
     else{
-        food.x = Math.floor(Math.random() * 15 +1) * box;
-        food.y = Math.floor(Math.random() * 15 +1) * box;
+        food.x = Math.floor(Math.random() * 13 +1) * box;
+        food.y = Math.floor(Math.random() * 13+1) * box;
     }
     
     let newHead ={
